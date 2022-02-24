54ba8c61cbe9220869539a0aa4372033e683c3df
Fergiaco <fernando.vgiacomini@gmail.com>
2022-02-24 13:01:28 -0300

    Kmeans Vinho

--- vinho.py
+++ vinho.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.datasets import load_wine
 from sklearn.svm import SVC
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
+vinho=load_wine()
+data = pd.DataFrame(data=vinho.data, columns=vinho.feature_names)
+print(data)
