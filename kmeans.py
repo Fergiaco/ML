e960823ae45836837aedf43ee5205530aa314291
Fer <fernando.vgiacomini@gmail.com>
2022-02-24 13:03:41 -0300

    Delete kmeans

--- kmeans.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-from sklearn.svm import SVC
-from sklearn.cluster import KMeans
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import StandardScaler
-
